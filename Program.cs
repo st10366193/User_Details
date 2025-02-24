b263860a8b8a746aded7102b019da9bef9767faf
Katlego Makhuba <128791831+st10366193@users.noreply.github.com>
2025-02-24 11:25:06 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Details
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Surname");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Enter Age");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDetailsEntered:");
+            Console.WriteLine("Name " + name);
+            Console.WriteLine("Surname " + surname);
+            Console.WriteLine("Age "+ age); 
+        }
+    }
+}
